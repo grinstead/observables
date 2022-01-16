c322baa41065b8baa76db267cbd1f9a15fb7cead
Joe Grinstead <npm@mailforce.net>
2022-01-16 13:16:15 -0500

    Add new output class

--- src/core.mjs
+++ src/core.mjs
@@ -60,6 +60,99 @@ function makeChild(handler, addedAfter) {
 }
 
 /**
+ * Represents where an invoked generator outputs to.
+ * @template T The type of each non-return iteration
+ * @template ReturnT The final return type
+ */
+class TxOutput {
+  constructor(handler) {
+    this.handler = handler;
+
+    /**
+     * This is an odd field
+     */
+    this._queueEnd = null;
+    this._nextIndex = 0;
+  }
+
+  /**
+   * Synchronously sends down the value
+   * @param {T} val
+   */
+  next(value) {
+    runEvent(this, { done: false, value });
+  }
+
+  /**
+   * Equivalent to {@link TxOutput.return} getting passed undefined
+   */
+  complete() {
+    this.return();
+  }
+
+  /**
+   * Ends the output with the given return value
+   * @param {ReturnT} returnVal
+   */
+  return(returnVal) {
+    runEvent(this, { done: true, value: returnVal });
+  }
+
+  error(error) {
+    runEvent(this, { done: "error", value: error });
+  }
+}
+
+/**
+ *
+ * @template T
+ * @template ReturnT
+ * @param {TxOutput<T,ReturnT>} output
+ * @param {Iteration<T,ReturnT>} iteration
+ * @returns
+ */
+function runEvent(output, iteration) {
+  const queueNode = {
+    nextEvent: null,
+    iteration,
+    index: output._nextIndex++,
+  };
+
+  const pending = output._queueEnd;
+  output._queueEnd = queueNode;
+
+  // if there is already code calling, then just add our iteration to the queue
+  if (pending) {
+    pending.nextEvent = queueNode;
+    return;
+  }
+
+  // There is not already a runEvent working, so we are the one in charge
+
+  // run through the queue, it is allowed to grow as we go
+  let active = queueNode;
+  while (active) {
+    const handler = output._handler;
+
+    if (!handler) {
+      // todo: drop silently
+      console.warn(`No handler`);
+      break;
+    }
+
+    const { index, iteration, nextEvent } = active;
+    if (iteration.done) {
+      output._handler = null;
+    }
+
+    handler(iteration, index);
+    active = nextEvent;
+  }
+
+  output._queueEnd = null;
+}
+
+/**
  * A stream represents ongoing data. The stream can be passed data as it comes
  * in through the {@link Stream.next} method.
  * @template T The type of each non-return iteration
@@ -75,16 +168,61 @@ class SyncStream {
     this._children = null;
 
     /**
-     * This is an odd field
-     */
-    this._queueEnd = null;
-
-    /**
      * If this is non-null, then the stream has finished
      * with that value
-     * @type {?Iteration}
+     * @type {boolean}
      */
-    this._finishedIter = null;
+    this._finished = false;
+
+    const output = new TxOutput();
+    this._output = output;
+
+    output.handler = (iteration, index) => {
+      const firstChild = this._children;
+
+      // skip to the first actual handler
+      let child = firstChild;
+      while (child && !child.handler) {
+        child = child.nextChild;
+      }
+
+      // actually remove the skipped handlers (null case implicitly handled)
+      if (child !== firstChild) {
+        stream._children = child;
+      }
+
+      let prevChild = null;
+      while (child) {
+        const { handler, addedAfter, nextChild } = child;
+        if (!handler) {
+          // Remove this child. Note that prevChild will not
+          // be null because we know that the first child we run
+          // on will have a handler
+          prevChild.nextChild = nextChild;
+          // do not update the prevChild
+        } else if (addedAfter) {
+          // the value is non-null only if the child was put in to the list while
+          // we processing previous events, so we do not call the handler
+
+          // if we have caught up to when the child was added, then get rid of the
+          // guard value
+          if (addedAfter === iteration) {
+            child.addedAfter = null;
+            prevChild = child;
+          } else {
+            // we know that anything after this child is just going to be ignored,
+            // might as well skip them
+            child = null;
+          }
+        } else {
+          // this code is guaranteed not to throw because it is wrapped elsewhere
+          handler(iteration, index);
+          prevChild = child;
+        }
+
+        child = nextChild;
+      }
+    };
   }
 
   /**
@@ -92,14 +230,7 @@ class SyncStream {
    * @param {T} val
    */
   next(val) {
-    if (this._finishedIter) {
-      // todo: throw error
-      console.warn(`Stream has already finished, cannot give next`);
-      return;
-    }
-
-    this._activeRun = true;
-    runHandlers(stream, makeIter(false, val));
+    this._output.next(val);
   }
 
   /**
@@ -114,27 +245,13 @@ class SyncStream {
    * @param {ReturnT} returnVal
    */
   return(returnVal) {
-    if (this._finishedIter) {
-      // todo: throw error
-      console.warn(`Stream has already finished, cannot return twice`);
-      return;
-    }
-
-    const finishedIter = makeIter(true, returnVal);
-    this._finishedIter = finishedIter;
-    runHandlers(stream, finishedIter);
+    this._finished = true;
+    this._output.return(returnVal);
   }
 
   error(error) {
-    if (this._finishedIter) {
-      // todo: throw error
-      console.warn(`Stream has already finished, cannot error`);
-      return;
-    }
-
-    const finishedIter = makeIter("error", error);
-    this._finishedIter = finishedIter;
-    runHandlers(stream, finishedIter);
+    this._finished = true;
+    this._output.error(error);
   }
 }
 
@@ -146,7 +263,7 @@ class SyncStream {
  * @param {Handler<T,ReturnT>} handler
  */
 function subscribe(stream, handler) {
-  const child = makeChild(handler, stream._queueEnd);
+  const child = makeChild(handler, stream._output._queueEnd);
 
   let children = stream._children;
   if (children && children.handler) {
@@ -167,80 +284,6 @@ function subscribe(stream, handler) {
   }
 
   // todo, trigger something
-}
-
-/**
- *
- * @param {SyncStream} stream
- * @param {Iteration<*,*>} iteration
- * @returns
- */
-function runHandlers(stream, iteration) {
-  const queueNode = {
-    nextEvent: null,
-    iteration,
-  };
-
-  const pending = stream._queueEnd;
-  stream._queueEnd = queueNode;
-
-  // if there is already code calling, then just add our iteration to the queue
-  if (pending) {
-    pending.nextEvent = queueNode;
-    return;
-  }
-
-  // There is not already a runHandlers working, so we are the one in charge
-
-  // run through the queue, it is allowed to grow as we go
-  for (let active = queueNode; active; active = queueNode.nextEvent) {
-    const firstChild = stream._children;
-
-    // skip to the first actual handler
-    let child = firstChild;
-    while (child && !child.handler) {
-      child = child.nextChild;
-    }
-
-    // actually remove the skipped handlers (null case implicitly handled)
-    if (child !== firstChild) {
-      stream._children = child;
-    }
-
-    let prevChild = null;
-    while (child) {
-      const { handler, addedAfter, nextChild } = child;
-      if (!handler) {
-        // Remove this child. Note that prevChild will not
-        // be null because we know that the first child we run
-        // on will have a handler
-        prevChild.nextChild = nextChild;
-        // do not update the prevChild
-      } else if (addedAfter) {
-        // the value is non-null only if the child was put in to the list while
-        // we processing previous events, so we do not call the handler
-
-        // if we have caught up to when the child was added, then get rid of the
-        // guard value
-        if (addedAfter === active) {
-          child.addedAfter = null;
-          prevChild = child;
-        } else {
-          // we know that anything after this child is just going to be ignored,
-          // might as well skip them and move onto the next event
-          child = null;
-        }
-      } else {
-        // this code is guaranteed not to throw because it is wrapped elsewhere
-        handler(active.iteration);
-        prevChild = child;
-      }
-
-      child = nextChild;
-    }
-  }
-
-  stream._queueEnd = null;
 }
 
 class AsyncIteration {
@@ -277,22 +320,20 @@ class AsyncGen {
   open(arg) {
     const closers = [];
 
-    let outputStream = new SyncStream();
+    let output = new TxOutput();
     let gen = this;
     let parent = gen._parent;
 
     while (parent) {
-      const inputStream = new SyncStream();
+      closers.push(gen._open(output));
 
-      closers.push(gen._open(inputStream, outputStream));
-
-      outputStream = inputStream;
+      output = new TxOutput();
       gen = parent;
       parent = parent._parent;
     }
 
     // actually start the output stream
-    closers.push(gen._open(outputStream, arg));
+    closers.push(gen._open(output, arg));
 
     return new AsyncIteration(closers);
   }
